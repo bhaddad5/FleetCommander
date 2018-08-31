a3c2e6c8195b658ae75039f7939f0c1e8b8d19c3
bhaddad5 <bhaddad5@gmail.com>
2018-08-30 23:57:17 -0700

    first commit!

---
+++ Assets/ControllerPointer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPointer : MonoBehaviour
+{
+	public GameObject BeamPrefab;
+	private GameObject beam;
+
+	private GameObject tracker;
+
+	void Start()
+	{
+		beam = Instantiate(BeamPrefab);
+		beam.transform.SetParent(transform, false);
+
+		tracker = new GameObject("Tracker");
+		tracker.transform.SetParent(transform, false);
+	}
+
+	private ShipController pickedUpShip;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(pickedUpShip == null)
+			PointAtShips();
+		else HandlePickedUpShip();
+	}
+
+	private void PointAtShips()
+	{
+		RaycastHit hit;
+		Physics.Raycast(new Ray(transform.position, transform.forward), out hit);
+
+		float dist = hit.distance;
+		if (hit.transform == null)
+			dist = 1000f;
+
+		beam.transform.localScale = new Vector3(.01f, .01f, dist);
+
+		ShipController pointedShip = hit.transform?.GetComponent<ShipController>();
+
+		if (pointedShip != null && SteamVR_Controller.Input((int) GetComponent<SteamVR_TrackedObject>().index).GetHairTriggerDown())
+		{
+			Debug.Log($"Picking up {pointedShip.gameObject.name}");
+
+			pickedUpShip = pointedShip;
+			pickedUpShip.ShowMovementRange();
+			tracker.transform.position = hit.transform.position;
+			tracker.transform.eulerAngles = hit.transform.eulerAngles;
+		}
+	}
+
+	private void HandlePickedUpShip()
+	{
+		beam.transform.localScale = new Vector3(0, 0, 0);
+
+		if (SteamVR_Controller.Input((int) GetComponent<SteamVR_TrackedObject>().index).GetHairTriggerUp())
+		{
+			Debug.Log("HIT!!!");
+			pickedUpShip.HideMovementRange();
+			pickedUpShip = null;
+		}
+		else
+		{
+			pickedUpShip.SetDesiredPos(tracker.transform.position, tracker.transform.eulerAngles);
+		}
+	}
+
+}
---
+++ Assets/ShipController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+	public float Speed = 0f;
+	private float maxXZDist => Speed;
+	private float maxYDist => Speed / 3f;
+
+	public GameObject DesiredPosTracker;
+
+	public GameObject MovementRangeDisplayPrefab;
+	private GameObject movementRangeDisplay;
+
+	void Start()
+	{
+		movementRangeDisplay = GameObject.Instantiate(MovementRangeDisplayPrefab);
+		movementRangeDisplay.transform.SetParent(transform, false);
+	}
+
+	public void ShowMovementRange()
+	{
+		DesiredPosTracker.SetActive(true);
+		movementRangeDisplay.SetActive(true);
+		movementRangeDisplay.transform.localScale = new Vector3(maxXZDist, maxYDist, maxXZDist);
+	}
+
+	public void HideMovementRange()
+	{
+		movementRangeDisplay.SetActive(false);
+	}
+
+	public void SetDesiredPos(Vector3 pos, Vector3 rot)
+	{
+		DesiredPosTracker.transform.eulerAngles = rot;
+
+		Vector3 vec = pos - transform.position;
+		Ray rayToDesiredPos = new Ray(transform.position, vec);
+		DesiredPosTracker.transform.position = rayToDesiredPos.GetPoint(Mathf.Min(Speed, vec.magnitude));
+	}
+}
